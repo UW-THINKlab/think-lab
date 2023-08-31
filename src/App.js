5fa77df5b3c3b36265a2d5a189920e104dd8752c
MiaPham2520 <mia2520@uw.edu>
2023-08-31 23:28:47 +0700

    trying to fix routing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/think-lab' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/people' element={<People />} />
         <Route path='/splashes' element={<Splashes />} />
